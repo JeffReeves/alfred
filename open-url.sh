03c151bc8bb708289462993a8307f0b2e9d517b1
JeffReeves <jeff@binary.run>
2020-09-18 02:57:07 -0700

    add URL opener

---
+++ open-url.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# purpose: open a URL with the default browser
+# author: Jeff Reeves
+
+# get input from user
+if [ -z ${1} ]; then 
+   echo "[ERROR] No URL provided"
+   echo "[HELP] Call this command with a quoted URL as a paramater"
+   exit 1
+fi
+
+# set URL
+URL="${1}"
+
+# output URL
+echo "[INFO] URL:"
+echo "${URL}"
+
+# open the URL
+echo "[TASK] Opening URL in the default browser ..."
+echo "[COMMAND] xdg-open \"${URL}\" || \
+sensible-browser \"${URL}\" || \
+x-www-browser \"${URL}\" || \
+gnome-open \"${URL}\""
+
+# run commands
+xdg-open "${URL}" || \
+sensible-browser "${URL}" || \
+x-www-browser "${URL}" || \
+gnome-open "${URL}"
+

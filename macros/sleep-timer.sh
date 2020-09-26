886657b85f3cc1ac07df40e7c744c967c876f934
JeffReeves <jeff@binary.run>
2020-09-26 04:02:05 -0700

    add sleep timer macro

---
+++ macros/sleep-timer.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# purpose: closes chrome and opens night clock
+# author: Jeff Reeves
+
+# get number of minutes to wait from the first parameter
+if [ -z ${1} ]; then
+    echo "[ERROR] Number of minutes not passed as a parameter"
+    echo "[HELP] Please re-run this command with a value (in minutes)"
+    exit 1
+else
+    MINUTES_TO_WAIT=${1}
+fi
+
+# wait for number of minutes
+~/alfred/wait.sh ${MINUTES_TO_WAIT}
+
+# close chromium
+~/alfred/close-chromium.sh
+
+# wait 5 seconds to ensure chromium is closed
+sleep 5
+
+# open the homepage URL for the night clock
+~/alfred/open-url.sh 'file:///home/jeff/homepage/index.html'
+
+# wait 5 seconds to ensure the page is open
+sleep 5
+
+# send F11 to fullscreen the homepage
+~/alfred/macros/fullscreen-homepage.sh

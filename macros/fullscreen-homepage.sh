a5687c8bb84811fe20d0bd956b43ba220e5294cf
Jeff Reeves <jeff@binary.run>
2020-09-28 01:20:33 -0700

    exit on failure to acquire window ID

--- macros/fullscreen-homepage.sh
+++ macros/fullscreen-homepage.sh
@@ -8,6 +8,7 @@ WINDOW_ID=$(xdotool search --name 'Homepage')
 # verify window ID was acquired
 if [ -z ${WINDOW_ID} ]; then
     echo "[ERROR] Unable to acquire Window ID for 'Homepage'"
+    exit 1
 else
     echo "[INFO] Window ID: ${WINDOW_ID}"
 fi
@@ -17,4 +18,3 @@ echo "[TASK] Activating window and sending F11 key ..."
 echo "[COMMAND] xdotool windowactivate ${WINDOW_ID} && xdotool key F11"
 xdotool windowactivate ${WINDOW_ID} && xdotool key F5
 xdotool windowactivate ${WINDOW_ID} && xdotool key F11
-

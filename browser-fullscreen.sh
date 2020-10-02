5ebed135182bee8779f68210ae2b1b5cdab69154
Jeff Reeves <jeff@binary.run>
2020-10-02 01:39:22 -0700

    Add check for existing fullscreen
    The script now attempts to get the screen size and window size,
    and checks if the window is fullscreened or not already.
    
    If the window is fullscreened, it skips sending the F11 fullscreen key.
    
    Regardless of browser window size, the 'f' key is still sent for
    YouTube videos -- as no current use cases would imply an already
    fullscreened YouTube video.
    
    Also added '[COMMAND]' output for each command that gets ran.

--- browser-fullscreen.sh
+++ browser-fullscreen.sh
@@ -12,6 +12,7 @@ fi
 
 # get window ID
 echo "[TASK] Getting Window ID for title '${WINDOW_SEARCH}' ..."
+echo "[COMMAND] xdotool search --name \".*${WINDOW_SEARCH}.*- (Google Chrome|Chromium|Mozilla Firefox)\"| head -n1"
 WINDOW_ID=$(xdotool search --name ".*${WINDOW_SEARCH}.*- (Google Chrome|Chromium|Mozilla Firefox)"| head -n1)
 
 # verify window ID was acquired
@@ -24,6 +25,7 @@ fi
 
 # get full window name
 echo "[TASK] Getting the full Window Title from Window ID ..."
+echo "[COMMAND] xdotool getwindowname ${WINDOW_ID}"
 WINDOW_TITLE=$(xdotool getwindowname ${WINDOW_ID})
 
 # verify Window Title was acquired
@@ -34,11 +36,59 @@ else
     echo "[INFO] Window Title: ${WINDOW_TITLE}"
 fi
 
-# activate window, then send F11 key to fullscreen
-echo "[TASK] Activating window and sending F11 key ..."
-echo "[COMMAND] xdotool windowactivate ${WINDOW_ID} && sleep 2 && xdotool key F11"
-xdotool windowactivate ${WINDOW_ID} && sleep 2 && xdotool key F11
-sleep 3
+# get full size of screen (ex. 1920x1080)
+echo "[TASK] Getting the screen's size in pixels using xdotool ..."
+echo "[COMMAND] xdotool getdisplaygeometry | tr ' ' 'x' | grep -Eo '[0-9]+x[0-9]+'"
+SCREEN_SIZE=$(xdotool getdisplaygeometry | tr ' ' 'x' | grep -Eo '[0-9]+x[0-9]+')
+# try using xwininfo instead of xdotool
+if [ -z "${SCREEN_SIZE}" ]; then
+    echo "[WARNING] Unable to acquire screen size with xdotool"
+    echo "[TASK] Attempting to use xwininfo to get screen size ..."
+    echo "[COMMAND] xwininfo -root | grep 'geometry' | grep -Eo '[0-9]+x[0-9]+'"
+    SCREEN_SIZE=$(xwininfo -root | grep 'geometry' | grep -Eo '[0-9]+x[0-9]+')
+fi
+
+# verify screen size was acquired
+if [ -z "${SCREEN_SIZE}" ]; then
+    echo "[WARNING] Unable to acquire screen size"
+    echo "[INFO] Defaulting to '1920x1080'"
+    SCREEN_SIZE='1920x1080'
+fi
+
+echo "[INFO] Screen Size: ${SCREEN_SIZE}"
+
+# check to see if any window is fullscreened already
+echo "[TASK] Getting the window's size in pixels using xdotool ..."
+echo "[COMMAND] xdotool getwindowgeometry \"${WINDOW_ID}\" | grep -i 'geometry' | grep -Eo '[0-9]+x[0-9]+'"
+WINDOW_SIZE=$(xdotool getwindowgeometry "${WINDOW_ID}" | grep -i 'geometry' | grep -Eo '[0-9]+x[0-9]+')
+
+# try using xwininfo instead of xdotool
+if [ -z "${WINDOW_SIZE}" ]; then
+    echo "[WARNING] Unable to acquire window size with xdotool"
+    echo "[TASK] Attempting to use xwininfo to get window size ..."
+    echo "[COMMAND] xwininfo -id \"${WINDOW_ID}\" | grep 'geometry' | grep -Eo '[0-9]+x[0-9]+'"
+    WINDOW_SIZE=$(xwininfo -id "${WINDOW_ID}" | grep 'geometry' | grep -Eo '[0-9]+x[0-9]+')
+fi
+
+# verify window size was acquired
+if [ -z "${WINDOW_SIZE}" ]; then
+    echo "[WARNING] Unable to acquire window size"
+    SCREEN_SIZE='Unknown'
+fi
+
+echo "[INFO] Window Size: ${WINDOW_SIZE}"
+
+# if window is not fullscreened
+if [ "${WINDOW_SIZE}" != "${SCREEN_SIZE}" ]; then
+    echo "[INFO] Window is not fullscreened"
+    # activate window, then send F11 key to fullscreen
+    echo "[TASK] Activating window and sending F11 key ..."
+    echo "[COMMAND] xdotool windowactivate ${WINDOW_ID} && sleep 2 && xdotool key F11"
+    xdotool windowactivate ${WINDOW_ID} && sleep 2 && xdotool key F11
+    sleep 3
+else 
+    echo "[INFO] Window is already fullscreened"
+fi
 
 # if a YouTube video, fullscreen it
 if [[ "${WINDOW_TITLE}" =~ '- YouTube -' ]]; then
